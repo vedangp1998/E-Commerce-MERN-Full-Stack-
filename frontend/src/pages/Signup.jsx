d258910e07f1c95f973c8d069ba12b505429a481
vedangp1998 <vedang.prajapati18@gmail.com>
2026-09-02 22:16:24 +0530

    addes Signup Page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,136 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-  return <div></div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-pink-100">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Create Your Account</CardTitle>
+            <CardDescription>
+              Enter given details below to create your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-3">
+                <div className="grid grid-cols-1 gap-4">
+                  <div className="grid gap-2">
+                    <Label htmlFor="firstName">First Name</Label>
+                    <Input
+                      id="firstName"
+                      name="firstName"
+                      type="text"
+                      placeholder="Vedang"
+                      required
+                      value={formData.firstName}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="grid gap-2">
+                    <Label htmlFor="lastName">Last Name</Label>
+                    <Input
+                      id="lastName"
+                      name="lastName"
+                      type="text"
+                      required
+                      placeholder="Prajapati"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input
+                      id="email"
+                      name="email"
+                      type="email"
+                      placeholder="m@example.com"
+                      required
+                      width="full"
+                      value={formData.email}
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                  </div>
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      name="password"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Createa a Password"
+                      required
+                      value={formData.password}
+                      onChange={handleChange}
+                    />
+                    {!showPassword ? (
+                      <EyeOff
+                        onClick={() => setShowPassword(true)}
+                        className="w-5 h-5 text-gray-700 absolute right-5 bottom-2"
+                      />
+                    ) : (
+                      <Eye
+                        onClick={() => setShowPassword(false)}
+                        className="w-5 h-5 text-gray-700 absolute right-5 bottom-2"
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full">
+              Signup
+            </Button>
+            <p className="text-gray-700 text-sm">
+              Already have an account?{" "}
+              <Link
+                to={"/login"}
+                className="hover:underline cursor-pointer text-pink-800"
+              >
+                Login
+              </Link>{" "}
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default Signup;
